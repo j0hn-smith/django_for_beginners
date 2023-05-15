3dd3ff34d0d09693361ea10704e6f565cf157bcc
Frederic Werner <frederic.werner@posteo.de>
2023-05-15 16:41:11 +0200

    finishes testing posts app

--- pages/tests.py
+++ pages/tests.py
@@ -4,35 +4,35 @@ from django.urls import reverse
 
 class HomePageTest(SimpleTestCase):
     def test_url_exists_at_correct_location(self):
-        response = self.client.get("/")
+        response = self.client.get("/pages/")
         self.assertEqual(response.status_code, 200)
 
     def test_url_available_by_name(self):
-        response = self.client.get(reverse("home"))
+        response = self.client.get(reverse("pages:home"))
         self.assertEqual(response.status_code, 200)
 
     def test_template_name_correct(self):
-        response = self.client.get(reverse("home"))
-        self.assertTemplateUsed(response, "home.html")
+        response = self.client.get(reverse("pages:home"))
+        self.assertTemplateUsed(response, "pages/home.html")
 
     def test_template_content(self):
-        response = self.client.get(reverse("home"))
+        response = self.client.get(reverse("pages:home"))
         self.assertContains(response, "<h1>Homepage</h1>")
 
 
 class AboutPageTest(SimpleTestCase):
     def test_url_exists_at_correct_location(self):
-        response = self.client.get("/about/")
+        response = self.client.get("/pages/about/")
         self.assertEqual(response.status_code, 200)
 
     def test_url_available_by_name(self):
-        response = self.client.get(reverse("about"))
+        response = self.client.get(reverse("pages:about"))
         self.assertEqual(response.status_code, 200)
 
     def test_template_name_correct(self):
-        response = self.client.get(reverse("about"))
-        self.assertTemplateUsed(response, "about.html")
+        response = self.client.get(reverse("pages:about"))
+        self.assertTemplateUsed(response, "pages/about.html")
 
     def test_template_content(self):
-        response = self.client.get(reverse("about"))
+        response = self.client.get(reverse("pages:about"))
         self.assertContains(response, "<h1>About Page</h1>")
--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
+from django.urls import reverse
+
+from posts.models import Post
+
 
 # Create your tests here.
+
+
+class PostTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.post = Post.objects.create(text="This is a test!")
+
+    def test_model_content(self):
+        self.assertEqual(self.post.text, "This is a test!")
+
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get("/posts/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_available_by_name(self):
+        response = self.client.get(reverse("posts:home"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_template_name_correct(self):
+        response = self.client.get(reverse("posts:home"))
+        self.assertTemplateUsed(response, "posts/post_list.html")
+
+    def test_template_conntent(self):
+        response = self.client.get(reverse("posts:home"))
+        self.assertContains(response, "This is a test!")
